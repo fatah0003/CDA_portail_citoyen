ab12608311460f7474a59fbd0210603bcc906554
Daniel-C <danielcourot@hotmail.com>
2025-07-02 12:26:49 +0200

    Create header and banniere

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,8 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/_header.css';
+import './styles/banner.css';
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
