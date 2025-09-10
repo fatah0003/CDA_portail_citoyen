57cffd46a5e21b3a12d095b0c05f547ad329725a
Daniel-C <danielcourot@hotmail.com>
2025-09-10 17:07:57 +0200

    change menu burger

--- back/var/cache/dev/twig/83/8328990d10320e4bcbdd182a492433bb.php
+++ back/var/cache/dev/twig/83/8328990d10320e4bcbdd182a492433bb.php
@@ -86,16 +86,46 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
                 <li><a href=\"#\">À propos</a></li>
                 <li><a href=\"#\">Nous contacter</a></li>
             </ul>
-            <a href=\"";
+            ";
         // line 25
-        yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_login");
-        yield "\" class=\"btn-connexion\">Connexion</a>
+        if ((CoreExtension::getAttribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 25, $this->source); })()), "user", [], "any", false, false, false, 25) && CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 25, $this->source); })()), "user", [], "any", false, false, false, 25), "infosUser", [], "any", false, false, false, 25))) {
+            // line 26
+            yield "                <div class=\"user-menu\">
+    <span class=\"btn-connexion-connect\">
+        ";
+            // line 28
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Twig\Extension\CoreExtension::capitalize($this->env->getCharset(), CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 28, $this->source); })()), "user", [], "any", false, false, false, 28), "infosUser", [], "any", false, false, false, 28), "firstName", [], "any", false, false, false, 28)), "html", null, true);
+            yield " ";
+            yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape(Twig\Extension\CoreExtension::upper($this->env->getCharset(), CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, CoreExtension::getAttribute($this->env, $this->source, (isset($context["app"]) || array_key_exists("app", $context) ? $context["app"] : (function () { throw new RuntimeError('Variable "app" does not exist.', 28, $this->source); })()), "user", [], "any", false, false, false, 28), "infosUser", [], "any", false, false, false, 28), "lastName", [], "any", false, false, false, 28)), "html", null, true);
+            yield "
+        <span class=\"online-dot\"></span>
+    </span>
+                    <div class=\"dropdown-menu\">
+                        <a href=\"";
+            // line 32
+            yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_logout");
+            yield "\">Se déconnecter</a>
+                    </div>
+                </div>
+            ";
+        } else {
+            // line 36
+            yield "                <a href=\"";
+            yield $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_login");
+            yield "\" class=\"btn-connexion\">Connexion</a>
+            ";
+        }
+        // line 38
+        yield "
+
+
+
         </nav>
     </div>
 </header>
 
 ";
-        // line 30
+        // line 46
         yield from $this->unwrap()->yieldBlock('javascripts', $context, $blocks);
         
         $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
@@ -118,10 +148,10 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
-        // line 31
+        // line 47
         yield "    ";
         yield from $this->unwrap()->yieldBlock('importmap', $context, $blocks);
-        // line 32
+        // line 48
         yield "    <script>
         document.addEventListener(\"DOMContentLoaded\", () => {
             const burger = document.querySelector(\".burger\");
@@ -134,6 +164,36 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
             }
         });
     </script>
+    <script>
+        let hideTimeout;
+
+        document.addEventListener('mouseover', (event) => {
+            const userMenu = event.target.closest('.user-menu');
+            if (userMenu) {
+                const dropdown = userMenu.querySelector('.dropdown-menu');
+                if (dropdown) {
+                    clearTimeout(hideTimeout);
+                    dropdown.style.display = 'flex';
+                }
+            }
+        });
+
+        document.addEventListener('mouseout', (event) => {
+            const userMenu = event.target.closest('.user-menu');
+            if (userMenu) {
+                const dropdown = userMenu.querySelector('.dropdown-menu');
+                if (dropdown) {
+                    hideTimeout = setTimeout(() => {
+                        dropdown.style.display = 'none';
+                    }, 500);
+                }
+            }
+        });
+    </script>
+
+
+
+
 ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -144,7 +204,7 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
         yield from [];
     }
 
-    // line 31
+    // line 47
     /**
      * @return iterable<null|scalar|\Stringable>
      */
@@ -188,7 +248,7 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  148 => 31,  125 => 32,  122 => 31,  99 => 30,  91 => 25,  84 => 21,  80 => 20,  72 => 15,  59 => 5,  55 => 4,  50 => 1,);
+        return array (  208 => 47,  155 => 48,  152 => 47,  129 => 46,  119 => 38,  113 => 36,  106 => 32,  97 => 28,  93 => 26,  91 => 25,  84 => 21,  80 => 20,  72 => 15,  59 => 5,  55 => 4,  50 => 1,);
     }
 
     public function getSourceContext(): Source
@@ -217,7 +277,23 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
                 <li><a href=\"#\">À propos</a></li>
                 <li><a href=\"#\">Nous contacter</a></li>
             </ul>
-            <a href=\"{{ path('app_login') }}\" class=\"btn-connexion\">Connexion</a>
+            {% if app.user and app.user.infosUser %}
+                <div class=\"user-menu\">
+    <span class=\"btn-connexion-connect\">
+        {{ app.user.infosUser.firstName|capitalize }} {{ app.user.infosUser.lastName|upper }}
+        <span class=\"online-dot\"></span>
+    </span>
+                    <div class=\"dropdown-menu\">
+                        <a href=\"{{ path('app_logout') }}\">Se déconnecter</a>
+                    </div>
+                </div>
+            {% else %}
+                <a href=\"{{ path('app_login') }}\" class=\"btn-connexion\">Connexion</a>
+            {% endif %}
+
+
+
+
         </nav>
     </div>
 </header>
@@ -236,6 +312,36 @@ class __TwigTemplate_2dc5d57e008b960b75a929bb93d2dee3 extends Template
             }
         });
     </script>
+    <script>
+        let hideTimeout;
+
+        document.addEventListener('mouseover', (event) => {
+            const userMenu = event.target.closest('.user-menu');
+            if (userMenu) {
+                const dropdown = userMenu.querySelector('.dropdown-menu');
+                if (dropdown) {
+                    clearTimeout(hideTimeout);
+                    dropdown.style.display = 'flex';
+                }
+            }
+        });
+
+        document.addEventListener('mouseout', (event) => {
+            const userMenu = event.target.closest('.user-menu');
+            if (userMenu) {
+                const dropdown = userMenu.querySelector('.dropdown-menu');
+                if (dropdown) {
+                    hideTimeout = setTimeout(() => {
+                        dropdown.style.display = 'none';
+                    }, 500);
+                }
+            }
+        });
+    </script>
+
+
+
+
 {% endblock %}
 ", "_partials/_header.html.twig", "C:\\Users\\Daniel\\Desktop\\tp_portail\\CDA_portail_citoyen\\back\\templates\\_partials\\_header.html.twig");
     }
