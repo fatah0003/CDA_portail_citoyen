6c70c4b87af60e58199b0a35c1b1f3f60e9e1528
fatah0003 <fatahutd@gmail.com>
2025-09-10 11:35:07 +0200

    react

---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Accueil du Portail Citoyen</h1>
+            <p>Bienvenue sur la plateforme où les citoyens peuvent proposer et voter des idées !</p>
+        </div>
+    );
+}
+
+export default Home;
---
+++ front/src/pages/Propositions.jsx
@@ -0,0 +1,10 @@
+function Propositions() {
+    return (
+        <div>
+            <h1>Liste des Propositions</h1>
+            <p>Ici s’afficheront toutes les propositions des citoyens.</p>
+        </div>
+    );
+}
+
+export default Propositions;
