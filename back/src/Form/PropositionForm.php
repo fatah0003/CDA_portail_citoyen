72dcdc3e75326e253b464f6bb0d6e19954cd4971
Ophelie <o.ferrand.42@gmail.com>
2025-09-19 01:32:48 +0200

    Fix .env.local after merge

--- back/src/Form/PropositionForm.php
+++ back/src/Form/PropositionForm.php
@@ -3,21 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Proposition;
-<<<<<<< HEAD
 use App\Enum\PropositionCategory;
+use App\Enum\PropositionStatus;
+use App\Form\PropositionFileTypeForm;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-=======
-use App\Form\PropositionFileTypeForm;
-use App\Enum\PropositionStatus;
-use App\Enum\PropositionCategory;
-use Symfony\Component\Form\AbstractType;
->>>>>>> feature/vich-symfonyV2
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Form\DataTransformer\PropositionCategoryArrayTransformer;
 
@@ -33,7 +27,6 @@ class PropositionForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-<<<<<<< HEAD
             ->add('title', TextType::class, [
                 'label' => 'Titre de la proposition',
                 'required' => true,
@@ -52,9 +45,7 @@ class PropositionForm extends AbstractType
                     'placeholder' => 'Décrivez votre proposition en détail',
                     'rows' => 5
                 ],
-=======
-            ->add('title')
-            ->add('description')
+            ])
             ->add('status', ChoiceType::class, [
                 'choices' => array_combine(
                     array_map(fn($status) => $status->name, PropositionStatus::cases()),
@@ -62,7 +53,6 @@ class PropositionForm extends AbstractType
                 ),
                 'choice_label' => fn($status) => $status->name,
                 'choice_value' => fn(?PropositionStatus $status) => $status?->value,
->>>>>>> feature/vich-symfonyV2
             ])
             ->add('category', ChoiceType::class, [
                 'label' => 'Catégorie',
@@ -70,20 +60,15 @@ class PropositionForm extends AbstractType
                 'choice_label' => fn($choice) => $choice instanceof PropositionCategory ? ucfirst($choice->value) : $choice,
                 'multiple' => true,
                 'expanded' => true,
-<<<<<<< HEAD
                 'label_attr' => ['class' => 'label-form'],
-                'attr' => [
-                    'class' => 'checkbox-group'
-                ],
-=======
+                'attr' => ['class' => 'checkbox-group'],
             ])
             ->add('propositionFiles', CollectionType::class, [
                 'entry_type' => PropositionFileTypeForm::class,
                 'allow_add' => true,
                 'allow_delete' => true,
-                'by_reference' => false, // obligatoire pour OneToMany
+                'by_reference' => false,
                 'prototype' => true,
->>>>>>> feature/vich-symfonyV2
             ]);
 
         $builder->get('category')->addModelTransformer($this->transformer);
