45a6234206d748c7ac9d2b62897e84411284cd5e
fatah0003 <fatahutd@gmail.com>
2025-10-03 17:04:16 +0200

    ajout du menu burger au header pour la version mobile, et hestion des bouton favoris, edit et delete

--- back/src/Controller/PropositionController.php
+++ back/src/Controller/PropositionController.php
@@ -60,7 +60,9 @@ class PropositionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_proposition_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_proposition_show', [
+                'id' => $proposition->getId(),
+            ], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('proposition/edit.html.twig', [
